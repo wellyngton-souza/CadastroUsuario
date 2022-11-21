db350285b9cb1afd68a47a6567c35d8b5c3589df
wellyngton <82477177+wellyngton-souza@users.noreply.github.com>
2022-11-20 21:15:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+function ativarPesquisa() {
+    var barra = document.getElementById('barra');
+    if (barra.style.visibility != 'visible')
+    {
+        barra.style.visibility = 'visible';
+    }
+    else {
+        barra.style.visibility = 'hidden';
+    }
+}
+
+function tema_escuro() {
+    var check = document.getElementById('check');
+    var body = document.getElementById('body');
+    var button1 = document.getElementById('button1');
+    var button2 = document.getElementById('button2');
+    var navbar = document.getElementById('navbar');
+    var label = document.getElementById('label');
+    var texbox = document.getElementById('texbox');
+    if (check.checked)
+    {
+        body.style.backgroundColor = 'rgb(40,40,40)';
+        body.style.color = 'white';
+        button1.style.color = 'white';
+        button2.style.color = 'white';
+        navbar.style.backgroundColor = 'rgb(93, 0, 112)';
+        label.style.border = 'solid 0.15rem';
+        label.style.padding = '0.5rem 1rem';
+        label.style.borderLeft = 'transparent';
+        label.style.borderRight = 'transparent';
+        label.style.borderTop = 'transparent';
+        texbox.style.color = 'white';
+    }
+    else {
+        body.style.backgroundColor = 'rgb(245,245,245)';
+        body.style.color = 'black';
+        button1.style.color = 'black';
+        button2.style.color = 'black';
+        navbar.style.backgroundColor = 'rgb(215, 24, 253)';
+        label.style.border = 'none';
+        texbox.style.color = 'black';
+    }
+}
